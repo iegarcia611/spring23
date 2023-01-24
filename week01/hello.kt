68102b7fcf6436d171df58afe0e018d3301b361d
iegarcia611 <123094811+iegarcia611@users.noreply.github.com>
2023-01-24 09:47:46 -0600

    Create hello.kt
    this is my first kotlin

---
+++ week01/hello.kt
@@ -0,0 +1,9 @@
+fun main() {
+    println("Hello, world!!!")
+    println("I am learning java ")
+    println("It is awesome!")
+    // This is my first kotlin file 
+    //ivan garcia 
+     
+    
+}
